7f452c37d86f9197d6fcfb93a9eda0463bf025b1
EvgeniGordeev <egordeev@conservis.ag>
2021-05-14 17:11:17 -0500

    gitignore

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -11,7 +11,6 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@Configuration
 @EnableAspectJAutoProxy
 @EnableTransactionManagement
 public class Application {
